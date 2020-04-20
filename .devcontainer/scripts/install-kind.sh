e1643783ca0cc8ec4ea45fc0ae7bf040b58307b8
Lawrence Gripper <info@grippers.co.uk>
2020-04-20 11:10:43 +0100

    Bump kind version test latestk8s on pr

--- .devcontainer/scripts/install-kind.sh
+++ .devcontainer/scripts/install-kind.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-linux-amd64
+curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
